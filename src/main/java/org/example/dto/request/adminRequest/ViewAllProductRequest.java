9dc76cc58f13915b338a45264a66d2ee83daec8d
olawhales <ajaditaoreed@gmail.com>
2025-01-14 09:22:15 +0100

    updates

---
+++ src/main/java/org/example/dto/request/adminRequest/ViewAllProductRequest.java
@@ -0,0 +1,15 @@
+package org.example.dto.request.goodsRequest.productRequest;
+
+import lombok.Data;
+
+
+@Data
+public class ProductRequest {
+//    private Long productId;
+//    private Long sellerId;
+//    private String productName;
+//    private String productDescription;
+//    private Double productPrice;
+//    private Integer productQuantity;
+
+}
